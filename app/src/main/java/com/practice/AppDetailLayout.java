d5497c9a95f7527778ec5d4649078c5b6184cda6
consul <2301488793@qq.com>
2016-12-28 17:48:22 +0800

    first commit

---
+++ app/src/main/java/com/practice/AppDetailLayout.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by gaofeng on 2016-12-28.
+ */
+
+public class AppDetailLayout extends LinearLayout {
+
+    public AppDetailLayout(Context context) {
+        super(context);
+    }
+
+    public AppDetailLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AppDetailLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/com/practice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+import android.graphics.drawable.ClipDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
