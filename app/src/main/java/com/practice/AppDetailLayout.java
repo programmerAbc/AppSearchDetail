8b6784fa34d45f4996d7e2d9d9bf84ab640d9d54
programmerAbc <2301488793@qq.com>
2016-12-29 23:40:06 +0800

    好像又布局错了

--- app/src/main/java/com/practice/AppDetailLayout.java
+++ app/src/main/java/com/practice/AppDetailLayout.java
@@ -4,29 +4,29 @@ import android.content.Context;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
+
 
 /**
  * Created by gaofeng on 2016-12-28.
  */
 
 public class AppDetailLayout extends LinearLayout {
-
+    public static final String TAG=AppDetailLayout.class.getSimpleName();
     View topFrameLayout;
     ImageView appCoverIv;
     ImageView appLogo;
-    View closeBar;
+    ImageView closeBtn;
     TextView appNameTv;
     View brifBar;
     View tabBar;
     NestedScrollView nestedScrollView;
     int nestedScrollHeight;
+    int topFrameLayoutMeasuredHeight=-1;
 
     public AppDetailLayout(Context context) {
         super(context);
@@ -43,9 +43,12 @@ public class AppDetailLayout extends LinearLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        nestedScrollHeight=topFrameLayout.getMeasuredHeight()+appNameTv.getMeasuredHeight()+brifBar.getMeasuredHeight()-closeBar.getMeasuredHeight();
+        nestedScrollHeight=topFrameLayout.getMeasuredHeight()- closeBtn.getMeasuredHeight();
         LinearLayout.LayoutParams nestedScrollViewLayoutParams= (LinearLayout.LayoutParams) nestedScrollView.getLayoutParams();
         nestedScrollViewLayoutParams.height=getMeasuredHeight()-tabBar.getMeasuredHeight();
+        if(topFrameLayoutMeasuredHeight<0){
+            topFrameLayoutMeasuredHeight=topFrameLayout.getMeasuredHeight();
+        }
         super.onMeasure(widthMeasureSpec,heightMeasureSpec);
     }
 
@@ -55,7 +58,7 @@ public class AppDetailLayout extends LinearLayout {
         topFrameLayout=findViewById(R.id.topFrameLayout);
         appCoverIv= (ImageView) findViewById(R.id.appCoverIv);
         appLogo= (ImageView) findViewById(R.id.appLogo);
-        closeBar=findViewById(R.id.closeBar);
+        closeBtn = (ImageView) findViewById(R.id.closeBtn);
         appNameTv= (TextView) findViewById(R.id.appNameTv);
         brifBar=findViewById(R.id.briefBar);
         tabBar=findViewById(R.id.tabBar);
@@ -69,19 +72,41 @@ public class AppDetailLayout extends LinearLayout {
 
     @Override
     public void scrollTo(int x, int y) {
-        y = y < 0 ? 0 : y;
+        //y = y < 0 ? 0 : y;
         y = y > nestedScrollHeight ? nestedScrollHeight : y;
         float scrollFactor = (float) y / nestedScrollHeight;
-        closeBar.setAlpha(scrollFactor);
-        closeBar.setTranslationY(y);
-        appCoverIv.setScaleX(1+scrollFactor);
-        appCoverIv.setScaleY(1+scrollFactor);
-        appCoverIv.setAlpha(1-scrollFactor*2);
+        closeBtn.setAlpha(  Math.max(0,Math.min(scrollFactor*3,1)));
+        closeBtn.setTranslationY(y);
+        appCoverIv.setAlpha(Math.max(0,Math.min(1-Math.abs(scrollFactor)*3,1)));
+        if(scrollFactor<0){
+            topFrameLayout.setBackgroundResource(android.R.color.transparent);
+            appCoverIv.setScaleX(1-scrollFactor);
+            appCoverIv.setScaleY(1-scrollFactor);
+            topFrameLayout.setTranslationY(y);
+            LinearLayout.LayoutParams topFrameLayoutLayoutParams= (LinearLayout.LayoutParams) topFrameLayout.getLayoutParams();
+            topFrameLayoutLayoutParams.height=topFrameLayoutMeasuredHeight-y;
+            topFrameLayout.requestLayout();
+        }else{
+            topFrameLayout.setBackgroundResource(android.R.color.white);
+            topFrameLayout.setTranslationY(0);
+            LinearLayout.LayoutParams topFrameLayoutLayoutParams= (LinearLayout.LayoutParams) topFrameLayout.getLayoutParams();
+            topFrameLayoutLayoutParams.height=topFrameLayoutMeasuredHeight;
+            topFrameLayout.requestLayout();
+
+        }
+
+
+
+        if(appCoverIv.getAlpha()==0f){
+             closeBtn.setBackgroundResource(android.R.color.white);
+        }else{
+            closeBtn.setBackgroundResource(android.R.color.transparent);
+        }
         super.scrollTo(x, y);
     }
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-        if ((dy > 0 && getScrollY() < nestedScrollHeight) || dy < 0 && getScrollY() > 0 && !ViewCompat.canScrollVertically(target, -1)) {
+        if ((dy > 0 && getScrollY() < nestedScrollHeight) || dy < 0  && !ViewCompat.canScrollVertically(target, -1)) {
             consumed[1] = dy;
             scrollBy(0, dy);
         }
