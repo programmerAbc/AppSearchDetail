708a5daed67dc94156e4db07c8323b57cb5b3901
consul <2301488793@qq.com>
2016-12-30 14:26:59 +0800

    进一步完善

--- app/src/main/java/com/practice/AppDetailLayout.java
+++ app/src/main/java/com/practice/AppDetailLayout.java
@@ -1,6 +1,9 @@
 package com.practice;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.NestedScrollView;
 import android.util.AttributeSet;
@@ -16,8 +19,8 @@ import android.widget.TextView;
  * Created by gaofeng on 2016-12-28.
  */
 
-public class AppDetailLayout extends LinearLayout {
-    public static final String TAG=AppDetailLayout.class.getSimpleName();
+public class AppDetailLayout extends LinearLayout implements NestedScrollingParent {
+    public static final String TAG = AppDetailLayout.class.getSimpleName();
     View topFrameLayout;
     ImageView appCoverIv;
     ImageView appLogo;
@@ -26,11 +29,10 @@ public class AppDetailLayout extends LinearLayout {
     View brifBar;
     View tabBar;
     NestedScrollView nestedScrollView;
-    int nestedScrollHeight;
-    int topFrameLayoutMeasuredHeight=-1;
+    int nestedScrollHeight = -1;
+    int closeScrollDistance = -1;
     Scroller scroller;
-
-
+    boolean finished = false;
 
     public AppDetailLayout(Context context) {
         super(context);
@@ -47,74 +49,91 @@ public class AppDetailLayout extends LinearLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        nestedScrollHeight=topFrameLayout.getMeasuredHeight()- closeBtn.getMeasuredHeight();
-        LinearLayout.LayoutParams nestedScrollViewLayoutParams= (LinearLayout.LayoutParams) nestedScrollView.getLayoutParams();
-        nestedScrollViewLayoutParams.height=getMeasuredHeight()-tabBar.getMeasuredHeight();
-        if(topFrameLayoutMeasuredHeight<0){
-            topFrameLayoutMeasuredHeight=topFrameLayout.getMeasuredHeight();
+        if (nestedScrollHeight < 0) {
+            nestedScrollHeight = topFrameLayout.getMeasuredHeight() - closeBtn.getMeasuredHeight();
         }
-        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        if (closeScrollDistance < 0) {
+            closeScrollDistance = getMeasuredHeight() - topFrameLayout.getMeasuredHeight();
+        }
+        LinearLayout.LayoutParams nestedScrollViewLayoutParams = (LinearLayout.LayoutParams) nestedScrollView.getLayoutParams();
+        nestedScrollViewLayoutParams.height = getMeasuredHeight() - tabBar.getMeasuredHeight();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        topFrameLayout=findViewById(R.id.topFrameLayout);
-        appCoverIv= (ImageView) findViewById(R.id.appCoverIv);
-        appLogo= (ImageView) findViewById(R.id.appLogo);
+        topFrameLayout = findViewById(R.id.topFrameLayout);
+        appCoverIv = (ImageView) findViewById(R.id.appCoverIv);
+        appLogo = (ImageView) findViewById(R.id.appLogo);
         closeBtn = (ImageView) findViewById(R.id.closeBtn);
-        appNameTv= (TextView) findViewById(R.id.appNameTv);
-        brifBar=findViewById(R.id.briefBar);
-        tabBar=findViewById(R.id.tabBar);
-        nestedScrollView= (NestedScrollView) findViewById(R.id.nestedScrollView);
-        scroller=new Scroller(getContext());
-        scroller.startScroll(0,-getContext().getResources().getDisplayMetrics().heightPixels,0,getContext().getResources().getDisplayMetrics().heightPixels,900);
+        closeBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doFinish(2000);
+            }
+        });
+        appNameTv = (TextView) findViewById(R.id.appNameTv);
+        brifBar = findViewById(R.id.briefBar);
+        tabBar = findViewById(R.id.tabBar);
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+        scroller = new Scroller(getContext());
+        scroller.startScroll(0, -getContext().getResources().getDisplayMetrics().heightPixels, 0, getContext().getResources().getDisplayMetrics().heightPixels, 900);
         invalidate();
     }
 
     @Override
     public void computeScroll() {
-        if(scroller.computeScrollOffset()){
-            scrollTo(0,scroller.getCurrY());
+        if (scroller.computeScrollOffset()) {
+            scrollTo(0, scroller.getCurrY());
             invalidate();
+        } else if (finished) {
+            ((Activity) getContext()).finish();
+            ((Activity) getContext()).overridePendingTransition(0, 0);
         }
     }
 
-    @Override
-    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
-        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
-    }
 
     @Override
     public void scrollTo(int x, int y) {
         //y = y < 0 ? 0 : y;
         y = y > nestedScrollHeight ? nestedScrollHeight : y;
         float scrollFactor = (float) y / nestedScrollHeight;
-        closeBtn.setAlpha(  Math.max(0,Math.min(scrollFactor*3,1)));
+        closeBtn.setAlpha(Math.max(0, Math.min(scrollFactor * 3, 1)));
         closeBtn.setTranslationY(y);
-        appCoverIv.setAlpha(Math.max(0,Math.min(1-Math.abs(scrollFactor)*3,1)));
-        if(scrollFactor<0){
+        appCoverIv.setAlpha(Math.max(0, Math.min(1 - Math.abs(scrollFactor) * 3, 1)));
+        if (scrollFactor < 0) {
             topFrameLayout.setBackgroundResource(android.R.color.transparent);
-            appCoverIv.setScaleX(1-scrollFactor*3);
-            appCoverIv.setScaleY(1-scrollFactor*3);
+            appCoverIv.setScaleX(1 - scrollFactor * 3);
+            appCoverIv.setScaleY(1 - scrollFactor * 3);
             appCoverIv.setTranslationY(y);
-        }else{
+        } else {
             topFrameLayout.setBackgroundResource(android.R.color.white);
             appCoverIv.setTranslationY(0);
         }
 
 
-
-        if(appCoverIv.getAlpha()==0f){
-             closeBtn.setBackgroundResource(android.R.color.white);
-        }else{
+        if (appCoverIv.getAlpha() == 0f) {
+            closeBtn.setBackgroundResource(android.R.color.white);
+        } else {
             closeBtn.setBackgroundResource(android.R.color.transparent);
         }
         super.scrollTo(x, y);
     }
+
+    //NestedScrollingParent
+    @Override
+    public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+        if (finished) {
+            return false;
+        }
+        scroller.abortAnimation();
+        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+    }
+
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
-        if ((dy > 0 && getScrollY() < nestedScrollHeight) || dy < 0  && !ViewCompat.canScrollVertically(target, -1)) {
+        if ((dy > 0 && getScrollY() < nestedScrollHeight) || dy < 0 && !ViewCompat.canScrollVertically(target, -1)) {
             consumed[1] = dy;
             scrollBy(0, dy);
         }
@@ -125,12 +144,46 @@ public class AppDetailLayout extends LinearLayout {
         if (getScrollY() > 0 && getScrollY() < nestedScrollHeight) {
             return true;
         } else {
-            return super.onNestedPreFling(target, velocityX, velocityY);
+            return false;
         }
     }
 
     @Override
-    public void onStopNestedScroll(View child) {
-        super.onStopNestedScroll(child);
+    public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
+        return false;
     }
+
+    @Override
+    public int getNestedScrollAxes() {
+        return ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+
+    @Override
+    public void onStopNestedScroll(View child) {
+        if (getScrollY() < -getContext().getResources().getDisplayMetrics().heightPixels / 2) {
+            doFinish(300);
+        } else if (getScrollY() < 0) {
+            scroller.startScroll(0, getScrollY(), 0, -getScrollY(), 300);
+            invalidate();
+        }
+    }
+
+    public void doFinish(int animationDuration) {
+        if (finished == false) {
+            finished = true;
+            scroller.startScroll(0, getScrollY(), 0, -getMeasuredHeight() - getScrollY(), animationDuration);
+            invalidate();
+        }
+    }
+
+
+    @Override
+    public void onNestedScrollAccepted(View child, View target, int nestedScrollAxes) {
+    }
+
+    @Override
+    public void onNestedScroll(View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+    }
+
+
 }
--- app/src/main/java/com/practice/MainActivity.java
+++ app/src/main/java/com/practice/MainActivity.java
@@ -8,10 +8,16 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+AppDetailLayout appDetailLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        appDetailLayout= (AppDetailLayout) findViewById(R.id.appDetailLayout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        appDetailLayout.doFinish(2000);
     }
 }
